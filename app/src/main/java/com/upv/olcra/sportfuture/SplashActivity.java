a274b7c1e04b393575e988d934a778b40fd768bf
olcra <olivernicholascraven@gmail.com>
2017-08-23 00:56:56 +0200

    SplashScreen

---
+++ app/src/main/java/com/upv/olcra/sportfuture/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.upv.olcra.sportfuture;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
